318eb5e2f801ced83c8017d81e5ffcfebd7a38e0
Rob Court <robbie.court@gmail.com>
2023-06-12 18:38:19 +0100

    Fix for image Datasets

--- finalStep.py
+++ finalStep.py
@@ -188,7 +188,7 @@ start = timeit.default_timer()
 print("Fixes for scRNAseq DataSets...")
 vc.nc.commit_list(statements=[
     'MATCH (n:DataSet) WHERE n.short_form STARTS WITH "FBlc" SET n:hasScRNAseq SET n:scRNAseq_DataSet',
-    'MATCH (n:DataSet)<-[:has_source]-(:Individual)-[:in_register_with]->(:Template) SET n:has_image',
+    'MATCH (n:DataSet)<-[:has_source]-(:Individual)<-[:depicts]-(:Individual)-[:in_register_with]->(:Template) SET n:has_image',
     'MATCH (a)-[r1:licence]->(l:License) MERGE (a)-[r2:has_license]->(l) ON CREATE SET r2=r1 SET r2.label="has_license" DELETE r1',
     'MATCH (primary)<-[:composed_primarily_of]-(c:Cluster)-[:has_source]->(ds:scRNAseq_DataSet) WHERE not primary:hasScRNAseq SET primary:hasScRNAseq'
 ])
